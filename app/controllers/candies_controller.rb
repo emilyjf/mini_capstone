5d524938644f5c2a95d80584a99f34f765ac1314
emilyjf <flanagan.emily.j@gmail.com>
2017-05-07 16:55:44 -0500

    Sunday

--- app/controllers/candies_controller.rb
+++ app/controllers/candies_controller.rb
@@ -1,6 +1,22 @@
 class CandiesController < ApplicationController
-  def candy_action
-    @candy = Candy.all
-    render 'candy.html.erb'
+  def index
+    @candies = Candy.all
+  end
+
+  def show
+    candy_id = params[:id]
+    @candy = Candy.find_by(id: candy_id)
+  end
+
+  def new 
+
+  end
+
+  def create
+    candy = Candy.new(name: params[:name],
+                      description: params[:description],
+                      price: params[:price]
+                      )
+    candy.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/candy' => 'candies#candy_action'
+  get '/' => 'candies#index'
+  get '/candies' => 'candies#index'
+  get '/candies/:id' => 'candies#show'
+  post '/candies' => 'candies#create'
+  get '/recipes/new' => 'recipes#new'
 end
