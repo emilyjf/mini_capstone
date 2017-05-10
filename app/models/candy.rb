800c1414e5a81d7aac31db91865ed84020291dd3
emilyjf <flanagan.emily.j@gmail.com>
2017-05-09 21:37:22 -0500

    Tuesday

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -1,2 +1,21 @@
 class Candy < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime('%b %d, %Y')
+  end
+
+  def sale_message
+    if price < 2
+      puts "Discount item!"
+    else
+      puts "Everyday value!"
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
